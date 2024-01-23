742b946d06068d3ab6e4b2d069e75d1489a46220
Bragakaii <bragaluciano.nan@gmail.com>
2024-01-23 16:53:26 -0300

    iniciado

---
+++ controller/functions.js
@@ -0,0 +1,16 @@
+var movies = require('../modulo/filmes.js')
+
+function getFilmes(){
+
+    const arrayFilmes = []
+
+    const jsonFilmes = {}
+
+    arrayFilmes
+    .forEach( function (getFilmes){
+        
+        
+    });
+}
+
+console.log(jsonFilmes)
