69517fffa238805466cd97f2bf23fa9cdd3dae84
Bragakaii <bragaluciano.nan@gmail.com>
2024-03-28 17:02:33 -0300

    dao corigido

--- model/DAO/usuarios.js
+++ model/DAO/usuarios.js
@@ -7,7 +7,7 @@ const selectAllUsuarios = async function(){
 
     try {
         
-        let sql = 'SELECT * FROM tbl_usuarios'
+        let sql = 'SELECT * FROM tbl_usuario'
         let rsUsuarios = await prisma.$queryRawUnsafe(sql)
         return rsUsuarios
 
