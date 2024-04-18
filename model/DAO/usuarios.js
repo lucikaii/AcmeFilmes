2159bc33b308d874518105684e3d7cbd6c70106a
Bragakaii <bragaluciano.nan@gmail.com>
2024-04-18 17:03:14 -0300

    ahhhhh

--- model/DAO/usuarios.js
+++ model/DAO/usuarios.js
@@ -48,7 +48,51 @@ const deleteUsuario = async function(id){
 
 // POST DE USUARIOS
 
+const insertNovoUsuario = async function(dadosUsuario){
 
+    try {
+
+        let sql
+        let idSQL
+
+        if(dadosUsuario.foto_perfil = null || dadosUsuario.foto_perfil == undefined || dadosUsuario == ''){
+
+            sql = `INSERT INTO tbl_usuario (nome, email, senha, foto_perfil) VALUES
+            (
+                '${dadosUsuario.nome}',
+                '${dadosUsuario.email}',
+                '${dadosUsuario.senha}',
+                null
+            )`
+    
+            idSQL = `SELECT cast(id AS DECIMAL) FROM tbl_usuario ORDER BY id DESC LIMIT 1`
+        } else {
+    
+            sql = `INSERT INTO tbl_usuario (nome, email, senha, foto_perfil) VALUES
+            (
+                '${dadosUsuario.nome}',
+                '${dadosUsuario.email}',
+                '${dadosUsuario.senha}',
+                '${dadosUsuario.foto_perfil}'
+            )`
+    
+            idSQL = `SELECT cast(id AS DECIMAL) FROM tbl_usuario ORDER BY id DESC LIMIT 1`
+        }
+    
+        let result = prisma.$executeRawUnsafe(sql)
+        let idResult = await prisma.$queryRawUnsafe(idSQL)
+    
+        if(result && idResult){
+            return result, idResult
+        } else {
+            return false
+        }
+        
+    } catch (error) {
+        return false
+    }
+
+}
 // PUT DE USUARIOS
 
 
